498141f035f52f68b734c7f894afed4a628af246
Rockydonthurtem <charrisq@hotmail.com>
2018-06-20 13:42:49 -0500

    finished

--- practice.js
+++ practice.js
@@ -121,12 +121,23 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 /*
   Write a function called divider that is given one argument, numbersArray.
-  Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
+  Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray)
+   and the second item in the Array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
-
-
+function divider (numbersArray){
+  var even = []
+  var odd = []
+  for (let i=0; i<numbersArray.length;i++){
+    if (numbersArray[i]%2===0){
+      even.push(numbersArray[i])}
+      else {
+      odd.push(numbersArray[i])}
+    }
+    var result = [even,odd];
+    return result
+  }
+ 
 
 ////////// PROBLEM 7 //////////
 
@@ -138,14 +149,24 @@ var getRandomArbitrary = function() {
 
 /* 
   var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
-  Above you're given a function that will return a random number between 0 and 30. There is also a commented out array full of numbers to help you visualize what your function will be receiving.
+  Above you're given a function that will return a random number between 0 and 30. 
+  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
   Write a function named finder that will take in an array as an argument.
   It will then  get a random number (by invoking getRandomArbitrary).
   Loop through the array to see if that random number is in the array. 
   If it is, return true, if it's not, return false
 */
 
-//Code Here
+function finder (arr){
+  getRandomArbitrary
+  for (i=0;i<arr.length;i++){
+    if (arr.length[i] === getRandomArbitrary){
+      return true
+    }else
+    {return false}
+  }
+  finder()
+}
 
 
 
